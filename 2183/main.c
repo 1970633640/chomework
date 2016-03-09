b716284b11e692f581b14065e72e9161795b070f
bgtest001 <q1970633640@gmail.com>
2016-03-09 19:11:09 +0800

    123

---
+++ 2183/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+int cmp1 ( const void *a, const void *b )
+{
+    return *(int *)a - *(int *)b;
+}
+int cmp2 ( const void *a, const void *b )
+{
+    return *(int *)b - *(int *)a;
+}
+int main()
+{
+int i,j,k,n,d,a[30][1000],b[30][1000];
+long long int ans[30]= {0};
+    scanf("%d",&n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&d);
+        for(k=0; k<d; k++)scanf("%d",&a[i][k]);
+        for(k=0; k<d; k++)scanf("%d",&b[i][k]);
+        qsort(a[i],d,sizeof(a[i][0]),cmp1);
+        qsort(b[i],d,sizeof(b[i][0]),cmp2);
+        ans[i]=0;
+        for(k=0; k<d; k++)ans[i]=ans[i]+(long long int)a[i][k]*b[i][k];
+    }
+    for(i=0; i<n; i++)printf("Case #%d: %I64d\n",i+1,ans[i]);
+    return 0;
+}
