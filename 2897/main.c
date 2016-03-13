54ee496df5995a60f8e0eb70062751fd65ff0832
bgtest001 <q1970633640@gmail.com>
2016-03-13 21:35:33 +0800

    123

--- 2897/main.c
+++ 2897/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 int main()
 {
-    printf("Hello world!\n");
+    char x[102],ans[100];
+    int i,j;
+    gets(x);
+
+
+
+
     return 0;
 }
