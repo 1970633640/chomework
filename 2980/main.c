6b59d88a33297caf5f0e16759aaf863fa00a1c24
bgtest001 <q1970633640@gmail.com>
2016-05-03 21:34:59 +0800

    123

--- 2980/main.c
+++ 2980/main.c
@@ -8,17 +8,25 @@ strncpy(ans,s+a,b-a+1);
 return atoi(ans);
 
 }
-int nine(int x)
+long long int nine(int x)
 {char ans[30]={'9'};
+int i;
+for(i=0;i<30;i++)ans[i]='9';
 ans[x]='\0';
     return atoi(ans);
 }
-int gcd(int a,int b)
+int gcd(int a,int b)	/* 2个数的公约数 */
 {
-    if (a<b) return gcd(b,a);
-    else if (b==0) return a;
-    else return gcd(b,a%b);
+	int t;
+	while(b)
+	{
+		t = a%b;
+		a = b;
+		b = t;
+	}
+	return a;
 }
+
 int main()
 {int a,b,c,lena,lenb,lenc;  //整数  小数 循环
    int n,i,k;
@@ -29,9 +37,12 @@ int main()
    {lenc=0;
        printf("case #%d:\n",i);
        gets(s);
-       int flag=0;
+       int flag=-1;
+        for(k=0;k<strlen(s);k++)
+               if(s[k]=='.'){flag=0;break;}
        for(k=0;k<strlen(s);k++)
-        if(s[k]=='[')flag=1;
+        if(s[k]=='['){flag=1;break;}
+              if(flag==-1){printf("%d\n",atoi(s));continue;}
       if(flag==1)
        for(k=0;k<strlen(s);k++)
        {
@@ -45,18 +56,20 @@ int main()
            if(s[k]=='.'){a=mid(s,0,k-1);lena=k;}
            if(s[k]=='\0'){b=mid(s,lena+1,k-1);lenb=k-lena-1;break;}
        }
+     //  printf("--%d [%d]   %d [%d]  %d [%d]\n",a,lena,b,lenb,c,lenc);
 int M,N;   // M/N
 if(flag==0){M= b+(long long int)(pow(10,lenb)+0.5)*a;N=(long long int)(pow(10,lenb)+0.5);
     int temp=gcd(M,N);M=M/temp;N=N/temp;
-    printf("%d/%d\n",M,N);
-continue;
+  if(N!=1) { printf("%d/%d\n",M,N);continue;}
+  else {printf("%d\n",M);continue;}
 }
 if(flag==1){
-M=(long long int)(pow(10,lenb)+0.5)*b -c;
-N=nine(lenc)*(pow(10,lenb)+0.5);
-M=M+a*N;
+
+N=nine(lenc)*(long long int)(pow(10,lenb)+0.5);
+M=c+N*a+b*nine(lenc);
    int temp=gcd(M,N);M=M/temp;N=N/temp;
-    printf("%d/%d\n",M,N);continue;
+  if(N!=1) { printf("%d/%d\n",M,N);continue;}
+  else {printf("%d\n",M);continue;}
 }
 
 
